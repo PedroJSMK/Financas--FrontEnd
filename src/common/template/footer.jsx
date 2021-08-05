d372eb0ebd572475a0128a58e3c0353f47169ec6
Pedro Marques <pedrokanagusto@hotmail.com>
2021-08-05 13:36:57 -0300

    FrontEnd Financas V1.0

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+        Desenvolvido &copy; 2020
+            <a href='https://github.com/PedroJSMK' target='_blank'> PJSMK - curso React</a>.
+        </strong>
+    </footer>
+)
